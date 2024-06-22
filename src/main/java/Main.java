328cc1c7874f3d47118db38b2c01c5798e824b02
Samekah <Lucidareverie@outlook.com>
2024-06-23 00:56:25 +0100

    added more label features

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,24 +17,29 @@ public class Main {
         label.setIcon(image);                                                            //sets label image
 
 //        swing by default puts label text to the right of an image, to change this we can do the following:
-        label.setHorizontalTextPosition(JLabel.CENTER);           //for horizontal setting the label can be left, centre or right
-        label.setVerticalTextPosition(JLabel.TOP);           //for horizontal setting the label can be top, centre or right
+        label.setHorizontalTextPosition(JLabel.CENTER);           //for horizontal setting for the label text can be left, centre or right
+        label.setVerticalTextPosition(JLabel.TOP);                //for vertical setting for the label text can be top, centre or bottom
+        label.setVerticalAlignment(JLabel.CENTER);                //for horizontal setting for the label text and image can be left, centre or right
+        label.setHorizontalAlignment(JLabel.CENTER);              //for vertical setting for the label text and image can be top, centre or bottom
 
 //        we can also change the font and background colour and style of the text. For setting the font, it follows (fontName,fontStyle,fontSize)
-//        we can also set the border colour and features, limit the size of out label
+//        we can also set the border colour and features, limit the size and position of our label
         label.setForeground(new Color(0x4B00C6));
         label.setBackground(new Color(0xFCBFDD));
         label.setOpaque(true);
         label.setFont(new Font("Book Antiqua",Font.PLAIN,24));
         label.setBorder(border);
+        label.setBounds(200,130,image.getIconWidth(),image.getIconHeight()+90);
+//        System.out.printf("Image height: %d, Image width: %d",image.getIconWidth(),image.getIconHeight());
 
 //        we can also set how far away the text is from our image using +/- values
         label.setIconTextGap(50);
 
-
 //        add to frame and make visible
         cf.add(label);
         cf.setVisible(true);
+//        this line sets the size of the frame to accommodate for all components added to the frame, we turn off code that handles sizing to use this
+//        cf.pack();
 
     }
 }
